2e25798953c8a551f50fa5467aee49f9e4aad25f
oliver <oliver@3a964c32-7461-4f60-ae82-ad05498e24dc>
2009-10-14 10:27:14 +0000

    small change in commend
    git-svn-id: https://svn.cocoanetics.com/myappsales/trunk@136 3a964c32-7461-4f60-ae82-ad05498e24dc

--- update_3_to_4.sql
+++ update_3_to_4.sql
@@ -1,4 +1,4 @@
-/* Script to update apps.db from schema version 2 to 3 */
+/* Script to update apps.db from schema version 3 to 4 */
 
 begin exclusive transaction;
 
@@ -8,6 +8,6 @@ UPDATE Country SET language="tr", app_store_id=143480 WHERE ISO2="TR";
 
 /* update schema_version */ 
 
-/* update meta set schema_version = 3; */
+/* update meta set schema_version = 4; */
 
 commit;
