a8e733c934339198343d8df942b0e96ec23697ea
oliver <oliver@3a964c32-7461-4f60-ae82-ad05498e24dc>
2009-11-23 20:45:49 +0000

    remove reports without dates
    git-svn-id: https://svn.cocoanetics.com/myappsales/trunk@198 3a964c32-7461-4f60-ae82-ad05498e24dc

--- update_5_to_6.sql
+++ update_5_to_6.sql
@@ -6,6 +6,15 @@ begin exclusive transaction;
 
 update country set name = 'Vietnam' where iso2 = 'VN';
 
+
+/* Remove reports without dates */
+
+delete from sale where report_id in (select id from report where from_date is null or until_date is null);
+
+delete from report where from_date is null or until_date is null;
+
+
+
 /* update schema_version */ 
 
 /*
